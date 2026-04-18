fbcc8add4de74f9eb86440af3eb84e3568124f76
JosuePerez4 <josuedanielpg@ufps.edu.co>
2026-04-18 14:23:05 -0500

    ADD: basic routing

---
+++ src/test/java/service/microservice/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package service.microservice.api_gateway;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
